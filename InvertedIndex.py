6aa56796f9cd87366f4c5fc1c0f676eb7b69efed
Humza Khawar Khan Tareen <htareen.bscs20seecs@student.nust.edu.pk>
2021-12-23 17:24:17 +0500

    fixed file opening and closing issues

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -42,7 +42,7 @@ class InvertedIndex:
      
             for i, InvIndBar in enumerate(InvInd1):
                 if not len(InvIndBar) == 0:
-                    filename = os.path.join(self.temp_path, f"{i:03}_inverted_{ntpath.basename(ForwardIndexPath)}")
+                    filename = os.path.join(self.temppath, f"{i:03}_inverted_{ntpath.basename(ForwardIndexPath)}")
 
                     with open(filename, 'wb+') as InvertedIndexData:
                         pickle.dump(InvIndBar, InvertedIndexData)
@@ -62,36 +62,37 @@ class InvertedIndex:
         InvIndTemp = os.listdir(self.temppath)
 
         for i in range(self.lexiconsize // self.barrelsize + 1):
-            concerned_indexes = [temp_index for temp_index in InvIndTemp if temp_index.startswith(f"{i:03}_inverted_")]
+            concerned_indexes = [temp_index for temp_index in InvIndTemp if temp_index.startswith(f"{i:003}_inverted")]
             
             if not len(concerned_indexes):
                 continue
 
-            filename = os.path.join(self.path, f"{i:03}_inverted")
+            filename = os.path.join(self.path, f"{i:003}_inverted")
 
             #creating empty dictionary
-            InvertedIndexDictionary = {}
-
+            InvInd2 = {}
+            
             if os.path.exists(filename):
-                with open(filename, 'rb') as InvertedIndexData2:
-                    InvInd2 = pickle.load( InvertedIndexData2)
+                if os.path.getsize(filename)>0 :  
+                    with open(filename, 'rb') as InvertedIndexData2:
+                        InvInd2 = pickle.load( InvertedIndexData2)
 
 
             #iterating through each
             for concerned_index in concerned_indexes:
-                with open(os.path.join(self.temp_path, concerned_index), 'rb') as TemporaryIndexData:
+                with open(os.path.join(self.temppath , concerned_index), 'rb') as TemporaryIndexData:
                     TempInd = pickle.load(TemporaryIndexData)
 
                     #iterating through each
                     for word_id in TempInd:
 
                         if word_id in InvInd2:
-                             InvInd2[word_id].update(TempInd[word_id])
+                            InvInd2[word_id].update(TempInd[word_id])
                         else:
-                             InvInd2[word_id] = TempInd[word_id]
+                            InvInd2[word_id] = TempInd[word_id]
 
 
-                os.remove(os.path.join(self.temp_path, concerned_index))
+                os.remove(os.path.join(self.temppath, concerned_index))
             
             with open (filename, 'wb') as InvertedIndexData3:
                 pickle.dump( InvInd2, InvertedIndexData3)
